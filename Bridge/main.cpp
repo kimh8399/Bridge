62a49947a3ef2d175f6a38650e16b0e6d89413d3
kimh8399 <Mustard12>
2018-01-21 12:32:32 -0500

    Initial file added
    Added the first files

---
+++ Bridge/main.cpp
@@ -0,0 +1,14 @@
+// Bridge Simulator
+// Huck Kim 2018
+//
+// Basic Bridge Program
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
